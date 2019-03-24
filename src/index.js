ffc49d4977df5df066b162a6e4347218d7cda844
anthonyd <38865632+AnthonyD0589@users.noreply.github.com>
2019-03-23 20:01:19 -0400

    all inputs mapped to the state

--- src/index.js
+++ src/index.js
@@ -3,24 +3,70 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class InputRow extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    from_hour: null,
+	    from_minute: null,
+	    to_hour: null,
+	    to_minute: null,
+	};
+	this.onInputChange = this.onInputChange.bind(this);
+    }
+    onInputChange(event) {
+	const value = event.target.value;
+	const name = event.target.name;
+	const id = event.target.id;
+	this.props.handleInputChange(id, name, value); 
+	if (name == 'from-hour') {
+	    this.setState({from_hour: value});
+	}
+	else if (name == 'from-minute') {
+	    this.setState({from_minute: value});
+	}
+	else if (name == 'to-hour') {
+	    this.setState({to_hour: value});
+	}
+	else {
+	    this.setState({to_minute: value});
+	}
+    }
   render() {
+    const id = this.props.id;
+ 
     return (
 	<div className = "input-row">
 	   <div className="from-row">
-		<input type = "number" name = "from-hour" min = "1" max = "12" />
-		<input type = "number" name = "from-minute" min = "0" max = "59" />       
-		
+		<input id = {id} value = {this.state.value} onChange = {this.onInputChange} type = "number" name = "from-hour" min = "1" max = "12" />
+		<input id = {id} value = {this.state.value} onChange = {this.onInputChange} type = "number" name = "from-minute" min = "0" max = "59" />       
 	   </div>        
-
 	    <div className="to-row">
-		<input type = "number" name = "to-hour" min = "1" max = "12" />
-		<input type = "number" name = "to-minute" min = "0" max = "59" />       
+		<input id = {id} value = {this.state.value} onChange = {this.onInputChange} type = "number" name = "to-hour" min = "1" max = "12" />
+		<input id = {id} value = {this.state.value} onChange = {this.onInputChange} type = "number" name = "to-minute" min = "0" max = "59" />       
 	    </div>
-       </div>        
-
+        </div>        
     );
   }
 }
+
+class Input extends React.Component {
+    
+    render() {
+	const inputTable = [];
+	const inputLen = this.props.input.length;
+    
+	for (let i = 0; i < inputLen; i++) {
+	    inputTable.push(<InputRow 
+		id = {i}
+		handleInputChange = {this.props.handleInputChange}
+		/>)
+	}
+	return(
+	    inputTable
+	);
+    }
+}
+
 
 class AddDay extends React.Component {
     render() {
@@ -32,8 +78,9 @@ class AddDay extends React.Component {
 	    </button>
 	);
     }
+}
 
-}class SubtractDay extends React.Component {
+class SubtractDay extends React.Component {
     render() {
 	return (
 	    <button
@@ -45,57 +92,70 @@ class AddDay extends React.Component {
     }
 }
 
-const CreateInputs = (props) => {
-    let inputTable = [];
-    let inputLen = props.input.length;
-    for (let i = 0; i < inputLen; i++) {
-	inputTable.push(RenderInputRow(0))
-    } 
-
-    return inputTable;
-}
-
-const RenderInputRow = (i) => {
-    return <InputRow />;
-} 
-
 class Calculator extends React.Component {
     constructor(props) {
 	super(props);
 	this.state = {
 	    input: [
-		{id:1, fromHour:null, fromMinute:null, toHour:null, toMinute:null}
+		{id:0, fromHour:null, fromMinute:null, toHour:null, toMinute:null}
 	    ]
 	}
+	this.onInputChange = this.onInputChange.bind(this);
     }
     
     AddDayHandleClick(i) {
 	const input_list = this.state.input.slice();
-	const id = this.state.input.length + 1;
+	const id = input_list.length;
 	const new_input = {id:id, fromHour: null, fromMinute:null, toHour:null, toMinute:null};
 	input_list.push(new_input);
 	this.setState({input: input_list});
-	console.log(this.state.input);
+	console.log(input_list);
     }
     
     SubtractDayHandleClick(i) {
 	const input_list = this.state.input.slice();
 	input_list.pop();
 	this.setState({input: input_list});
-	console.log(this.state.input);
+	console.log(input_list);
     }
     
-      renderaddDayButton(i) {
+      renderaddDayButton() {
 	  return<AddDay 
-	      onClick = {() => this.AddDayHandleClick(i)}
+	      onClick = {() => this.AddDayHandleClick()}
 	  />;
       }
 
-      renderSubtractDayButton(i) {
+      renderSubtractDayButton() {
 	  return<SubtractDay 
-	      onClick = {() => this.SubtractDayHandleClick(i)}
+	      onClick = {() => this.SubtractDayHandleClick()}
 	  />;
       }
+    onInputChange(id, name, value) {
+	let inputs = [...this.state.input];
+	let input = {...inputs[id]};
+	if (name == 'from-hour') {
+	    input.fromHour = value;
+	}
+	else if (name == 'from-minute') {
+	    input.fromMinute = value;
+	}
+	else if (name == 'to-hour') {
+	    input.toHour = value;
+	}
+	else {
+	    input.toMinute = value;
+	}
+	inputs[id] = input;
+	this.setState({input: inputs});
+	console.log(inputs);
+    }
+	
+	renderInput() {
+	    return<Input
+		input = {this.state.input}
+		handleInputChange={this.onInputChange}
+	    />;
+	}
 
   render() {
     const total = '0';
@@ -104,11 +164,11 @@ class Calculator extends React.Component {
       <div>
         <div className="total">{total}</div>
 	<div className="addDay">
-	    {this.renderaddDayButton(0)} 
-	    {this.renderSubtractDayButton(0)} 
+	    {this.renderaddDayButton()} 
+	    {this.renderSubtractDayButton()} 
 	</div>
 	<div className="inputs">    
-	    <CreateInputs input={this.state.input} />
+	    {this.renderInput()} 
         </div>
       </div>
     );
