04e8f9303d642f04528ced7065ecf05b03944b68
anthonyd <38865632+AnthonyD0589@users.noreply.github.com>
2019-03-24 15:32:54 -0400

    Updates

--- precache-manifest.4690b7de8081dd29c4a293fbe376324c.js
+++ precache-manifest.4690b7de8081dd29c4a293fbe376324c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Pay-Calculator/static/js/runtime~main.a85f4b7c.js"
   },
   {
-    "revision": "efb3c802fd50ad24d2eb",
-    "url": "/Pay-Calculator/static/js/main.0e901782.chunk.js"
+    "revision": "848fda1a2cf7b97358fd",
+    "url": "/Pay-Calculator/static/js/main.6ae33b9a.chunk.js"
   },
   {
     "revision": "c3081561e5f2ba454038",
     "url": "/Pay-Calculator/static/js/2.0bc3dbf2.chunk.js"
   },
   {
-    "revision": "efb3c802fd50ad24d2eb",
+    "revision": "848fda1a2cf7b97358fd",
     "url": "/Pay-Calculator/static/css/main.b870fec1.chunk.css"
   },
   {
-    "revision": "fbe1f9ea5ca335b69b5984083530a0c5",
+    "revision": "bd02e2da6a95bfd51e27e4f54ef41a48",
     "url": "/Pay-Calculator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Pay-Calculator/precache-manifest.9813af4fba31a74754999ce2329aa27b.js"
+  "/Pay-Calculator/precache-manifest.4690b7de8081dd29c4a293fbe376324c.js"
 );
 
 workbox.clientsClaim();
